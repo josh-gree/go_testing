92edabca39824190c7aadf0a5174da1967defacc
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-30 18:37:00 +0100

    blah again..

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/labstack/echo"
-)
-
 func foo(x int) int {
 	return 2 * x
 }
 
 func main() {
-	fmt.Println("Hello")
 
-	e := echo.New()
-	e.Start(":9000")
 }
