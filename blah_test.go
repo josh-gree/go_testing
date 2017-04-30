3a5c422f78721498eda9612f1d8621c69a8e01cf
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-30 20:11:48 +0100

    Added some tests

--- blah_test.go
+++ blah_test.go
@@ -1,14 +1,17 @@
 package blah
 
-import (
-	"fmt"
-	"testing"
-)
-
-var tests = []uint64{1, 1, 2, 3, 5, 8, 13, 21, 34, 55}
+import "testing"
 
 func TestFoo(t *testing.T) {
-	fn := Foo(23)
-	fmt.Println(fn)
+	x := Foo(2)
+	if x != 4 {
+		t.Error("Expected 4 got ", x)
+	}
+}
 
+func TestBar(t *testing.T) {
+	x := Bar([]float64{1.1, 2.4})
+	if x != 3.5 {
+		t.Error("Expected 3.5 got ", x)
+	}
 }
