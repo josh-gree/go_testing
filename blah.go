660125e09e6e20d367cc04bf7151b4957ba67429
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-30 20:07:00 +0100

    Added new funtion and some docs

--- blah.go
+++ blah.go
@@ -1,5 +1,15 @@
 package blah
 
+// Foo multiplies by 2
 func Foo(x int) int {
 	return 2 * x
+}
+
+// Bar returns the sum of a slice of floats
+func Bar(x []float64) float64 {
+	out := 0.0
+	for _, num := range x {
+		out += num
+	}
+	return out
 }
